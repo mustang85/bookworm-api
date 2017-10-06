5966fa964fe745f43ae965a0249452b85c256642
mustang85 <sergeant-kir@mail.ru>
2017-10-06 09:17:08 +0300

    corrected end and User

--- src/models/User.js
+++ src/models/User.js
@@ -1,40 +1,45 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import uniqueValidator from 'mongoose-unique-validator';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import uniqueValidator from "mongoose-unique-validator";
 
-const schema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    index: true,
-    unique: true
+// TODO: add uniqueness and email validations to email field
+const schema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+      unique: true
+    },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, default: false },
+    confirmationToken: { type: String, default: "" }
   },
-  passwordHash: { type: String, required: true },
-  confirmed: { type: Boolean, default: false },
-  confirmationToken: { type: String, default: '' }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 schema.methods.isValidPassword = function isValidPassword(password) {
-  return bcrypt.compareSync(password, this.passwordHash)
-}
+  return bcrypt.compareSync(password, this.passwordHash);
+};
 
 schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
-}
+};
 
 schema.methods.setConfirmationToken = function setConfirmationToken() {
   this.confirmationToken = this.generateJWT();
-}
+};
 
 schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
-  return `${process.env.HOST}/confirmation/${this.setConfirmationToken}`;
-}
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+};
 
 schema.methods.generateResetPasswordLink = function generateResetPasswordLink() {
-  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`
-}
+  return `${process.env
+    .HOST}/reset_password/${this.generateResetPasswordToken()}`;
+};
 
 schema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
@@ -52,7 +57,7 @@ schema.methods.generateResetPasswordToken = function generateResetPasswordToken(
       _id: this._id
     },
     process.env.JWT_SECRET,
-    { expiresIn: '1h' }
+    { expiresIn: "1h" }
   );
 };
 
@@ -61,9 +66,9 @@ schema.methods.toAuthJSON = function toAuthJSON() {
     email: this.email,
     confirmed: this.confirmed,
     token: this.generateJWT()
-  }
-}
+  };
+};
 
-schema.plugin(uniqueValidator, { message: 'This email is already taken' });
+schema.plugin(uniqueValidator, { message: "This email is already taken" });
 
-export default mongoose.model('User', schema);
+export default mongoose.model("User", schema);
